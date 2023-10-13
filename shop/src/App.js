7b2139845834e912266bb735de02d24b40d836b4
Huluette <leana.beuvant@gmail.com>
2023-10-13 15:08:29 +0200

    affichage des données

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,21 +1,17 @@
-import { useSelector } from 'react-redux';
 import './App.css';
 import Shop from './components/shop';
 
-
 function App() {
-
-  const article = useSelector((state) => state.productReducer);
 
   return (
     <div className="App">
       <header className="App-header">
-      <h1>ShopTendance</h1>
+        <h1>ShopTendance</h1>
       </header>
       <section className="App-section">
-      <Shop/>
+        <Shop/>
       </section>
-      </div>
+    </div>
   );
 }
 
--- shop/src/components/shop.js
+++ shop/src/components/shop.js
@@ -1,23 +1,30 @@
-import React from "react";
+ import React from "react";
+ import { useSelector } from "react-redux";
 
+ function Shop(props) {
 
-let products = new Map();
+  const product = useSelector((state) => state.productReducer);
 
-function Shop(props) {
-
- const product = useSelector((state) => state.productReducer);
-
-  return (
+   return (
     <div className="container-card">
-      <h2 className="sous-titre">Articles</h2>
-      <ul className="content-card">
-        {product.map((product, i) => (
-          <shop key={i} product={product} />
-        ))}
-      </ul>
-    </div>
-  );
+    <h2 className="sous-titre">Articles</h2>
+    <ul className="content-card">
+      {product.map((product, i) => (
+        <li key={i}>
+          <shop product={product} /> 
+          <p>{product.title}</p>
+          <p>{product.description}</p>
+          <p>{product.categories[0]}, {product.categories[1]}</p>
+          <p>{product.basePrice} €</p>
+          <p>{product.salePrice} €</p>
+          <img src={product.imageUrl}></img>
 
-}
+        </li>
+      ))}
+    </ul>
+  </div>
+   );
 
-export default Shop;
+ }
+
+ export default Shop;
