c0f82a4ca6af4caf11171ed141b8958513b03edd
Huluette <leana.beuvant@gmail.com>
2023-10-13 10:39:58 +0200

    retour devTools, recup api ok

--- shop/src/index.js
+++ shop/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { getProducts } from './actions/product.action';
 import { createRoot } from 'react-dom/client';
+import rootReducer from "./reducers";
 
 const store = configureStore({
     reducer: rootReducer,
