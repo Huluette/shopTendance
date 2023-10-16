9ab260b0a1d1f68e8ef90f6c56c63f49621f336a
Huluette <leana.beuvant@gmail.com>
2023-10-16 09:33:06 +0200

    utilisation méthode isEmpty

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
-import Card from './components/card';
+import Shop from './components/shop';
+import { isEmpty } from './components/card';
+import { useSelector } from 'react-redux';
+
 
 function App() {
+
+const article = useSelector((state) => state.productReducer);
 
   return (
     <div className="App">
@@ -9,7 +14,9 @@ function App() {
         <h1>ShopTendance</h1>
       </header>
       <section className="App-section">
-        <Card/>
+        {!isEmpty(article) && article.map((product, i) => (
+          <Shop key={i} product={product} />
+        ))}
       </section>
     </div>
   );
--- shop/src/components/card.js
+++ shop/src/components/card.js
@@ -1,45 +1,7 @@
-import React from "react";
-import Shop from '../components/shop';
-
-const cardContainer = document.getElementById('content-card');
-// const filteredProducts = document.getElementById('filtered-products');
-
-let productsJSON;
-
-fetch('http://localhost:3000/products')
-    .then(response => response.json())
-    .then(data => {
-        productsJSON = data;
-        container(productsJSON.products);
-    })
-    .catch(error => {
-        console.error('Erreur de chargement :', error);
-    })
-
-function container() {
-
-    return (
-
-        // cardContainer.innerHTML = "";
-        cardContainer.forEach(function (card) {
-            let newCard = document.createElement('div');
-
-            newCard.classList.add('.card');
-            
-                <div className="container-card">
-                    <h2 className="sous-titre">Articles</h2>
-                    <div className="content-card">
-                        <Shop />
-                        {cardContainer.appendChild(card)}
-                    </div>
-                </div>
-            
-
-        }
-        )
-
-    )
-}
-
-
-export default container;
+export const isEmpty = (value) => {
+    return ( value === undefined || 
+    value === null || 
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0)
+    );    
+};
