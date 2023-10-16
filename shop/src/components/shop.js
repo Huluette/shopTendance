754c83c3d6d5a36d738b4a29ae1db10bdcdad98d
Huluette <leana.beuvant@gmail.com>
2023-10-16 10:44:52 +0200

    en cours d'ajout d'un nouvel article

--- shop/src/components/shop.js
+++ shop/src/components/shop.js
@@ -1,26 +1,24 @@
- import React from "react";
- import { useSelector } from "react-redux";
+import React from "react";
 
- function Shop(props) {
-
-  const product = useSelector((state) => state.productReducer);
-
-   return (
-    <div className="card">
-      {product.map((product, i) => (
-        <div key={i}>
-          <shop product={product} /> 
+function Shop(props) {
+  const { product } = props;
+  return (
+      <div className="produit">
+        <div className="image">
+          <img src={product.imageUrl}></img>
+        </div>
+        <div className="description">
           <p>{product.title}</p>
           <p>Description: {product.description}</p>
           <p>Catégorie: {product.categories[0]}, {product.categories[1]}</p>
           <p>Prix: {product.basePrice} €</p>
           <p>En solde: {product.salePrice} €</p>
-          <img src={product.imageUrl}></img>
         </div>
-      ))}
-    </div>
-   );
 
- }
+      </div>
 
- export default Shop;
+  );
+
+};
+
+export default Shop;
--- shop/src/reducers/product.reducer.js
+++ shop/src/reducers/product.reducer.js
@@ -1,6 +1,6 @@
 import { GET_PRODUCTS, ADD_PRODUCT } from "../actions/product.action";
 
-const initialState = {};
+const initialState = { };
 
 export default function productReducer(state = initialState, action) {
     switch (action.type) {
