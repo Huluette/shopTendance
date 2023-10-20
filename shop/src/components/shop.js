739781d54d3a10ead82575cf3719c48aeb60c4d9
Huluette <leana.beuvant@gmail.com>
2023-10-20 14:40:37 +0200

    modification target et en cours de la validation

--- shop/src/components/shop.js
+++ shop/src/components/shop.js
@@ -26,48 +26,70 @@ function Shop(props) {
       content: editContent,
     };
 
-    dispatch(editProduct(product));
+    dispatch(editProduct(postData));
     setEditToggle(false);
 
   }
 
   return (
 
-    <div className="produit">
-      {!isEmpty(product) && (
-        <div className="contain-edit-delete">
-          <div className="edit" alt="edit" onClick={() => setEditToggle(!editToggle)} />
-          <div className="delete" alt="delete" onClick={() => dispatch(deleteProduct(product.id))} />
-        </div>
-      )}
+    <>
 
-      <div className="image">
-        <img alt="URL" src={product.imageUrl} />
-      </div>
-      <div className="description">
-        <p>{product.title}</p>
-        <p>Description: {product.description}</p>
-        <p>Catégorie: {product.categories}</p>
-        <p>Prix: {product.basePrice} €</p>
-        <p>En solde: {product.salePrice} €</p>
-      </div>
+      <div className="">
 
-      {editToggle && (
-        <div className="produit" onSubmit={handleEditToggle} autoFocus="true" defaultValue={product} onChange={(e) => setEditContent(e.target.value)}>
-          <div className="image">
-            <img alt="URL" src={product.imageUrl} />
+
+        {!editToggle && (
+
+          <div className="produit">
+            {!isEmpty(product) && (
+              <div className="contain-edit-delete">
+                <div className="edit" alt="edit" onClick={() => setEditToggle(!editToggle)} />
+                <div className="delete" alt="delete" onClick={() => dispatch(deleteProduct(product.id))} />
+              </div>
+            )}
+            <div className="image">
+              <img alt="URL" src={product.imageUrl} />
+            </div>
+            <div className="description">
+              <p>{product.title}</p>
+              <p>Description: {product.description}</p>
+              <p>Catégorie: {product.categories}</p>
+              <p>Prix: {product.basePrice} €</p>
+              <p>En solde: {product.salePrice} €</p>
+            </div>
           </div>
-          <div className="description">
-            <p>{product.title}</p>
-            <p>Description: {product.description}</p>
-            <p>Catégorie: {product.categories}</p>
-            <p>Prix: {product.basePrice} €</p>
-            <p>En solde: {product.salePrice} €</p>
+        )}
+
+
+        {editToggle && (
+          <div className="produit">
+            <div className="image" onSubmit={handleEditToggle} autoFocus="true" defaultValue={product} onChange={(e) => setEditContent(e.target.value)}>
+              <img alt="URL" src={product.imageUrl} />
+            </div>
+            <div className="description editer">
+              <input defaultValue={product.title} placeholder="Titre..." required/>
+              <input defaultValue={product.description} placeholder="Description..." required/>
+              <select defaultValue={product.categories} placeholder="Choisissez une catégorie" required>
+                <option>Accessoires</option>
+                <option>Chaussures</option>
+                <option>Vêtements</option>
+                <option>Sport</option>
+                <option>Homme</option>
+                <option>Femme</option>
+              </select>
+              <input type="number" defaultValue={product.basePrice} placeholder="Prix de vente" required/>
+              <input type="number" defaultValue={product.salePrice} placeholder="Prix en solde" required/>
+              <input className="submitvalidate" type="submit" value="Valider modifiaction" placeholder="Valider modification"/>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-)};
+
+        )}
+
+
+      </div>
+    </>
+  )
+};
 
 
 export default Shop;
